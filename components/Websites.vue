4a80cb03d46c7eed085a5e3487c066635927d090
yechao <1151062047@qq.com>
2025-06-10 17:54:04 +0800

    feat: 添加枚举

--- components/Websites.vue
+++ components/Websites.vue
@@ -1,28 +1,12 @@
 <template>
   <div class="websites">
-    <img :src="website.icon" @click="handleOpenUrl(website.path)" v-for="(website, index) in websiteList" :key="index"/>
+    <img :src="website.icon" @click="handleOpenUrl(website.path)" v-for="(website, index) in WEBSITES" :key="index"/>
   </div>
 </template>
 
 <script setup>
-import Instagram from "@/assets/svg/instagram.svg";
-import Redbook from "@/assets/svg/redbook.svg";
-import Weibo from "@/assets/svg/weibo.svg";
+import { WEBSITES } from "@/const/enum.ts";
 
-const websiteList = [
-  {
-    icon: Instagram,
-    path: "https://www.instagram.com/tu1ko0o0_ss/",
-  },
-  {
-    icon: Redbook,
-    path: "https://www.xiaohongshu.com/user/profile/5f3e257100000000010070c5",
-  },
-  {
-    icon: Weibo,
-    path: "https://weibo.com/u/5655409705",
-  },
-];
 
 const handleOpenUrl = (url) => {
     window.open(url, '_blank')
--- const/enum.ts
+++ const/enum.ts
@@ -1,9 +1,33 @@
+import Instagram from "@/assets/svg/instagram.svg";
+import Redbook from "@/assets/svg/redbook.svg";
+import Weibo from "@/assets/svg/weibo.svg";
+
+// 语言
 export const enum LANGUAGE_TYPE {
-    CHINESE = 'zh',
-    ENGLISH = 'en'
+  CHINESE = "zh",
+  ENGLISH = "en",
 }
 
-export const LANGUAGE_TYPE_LABEL  = {
-    [LANGUAGE_TYPE.CHINESE]: '简体中文',
-    [LANGUAGE_TYPE.ENGLISH]: 'English'
-}
+export const LANGUAGE_TYPE_LABEL = {
+  [LANGUAGE_TYPE.CHINESE]: "简体中文",
+  [LANGUAGE_TYPE.ENGLISH]: "English",
+};
+
+//网站
+export const WEBSITES = [
+  {
+    title: 'Instagram',
+    icon: Instagram,
+    path: "https://www.instagram.com/tu1ko0o0_ss/",
+  },
+  {
+    title: 'Redbook',
+    icon: Redbook,
+    path: "https://www.xiaohongshu.com/user/profile/5f3e257100000000010070c5",
+  },
+  {
+    title: 'Weibo',
+    icon: Weibo,
+    path: "https://weibo.com/u/5655409705",
+  },
+];
